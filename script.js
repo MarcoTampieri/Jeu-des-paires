b2417ec0389ea50e31a715433929ab8278257d70
Marco <mcg.tampieri@gmail.com>
2018-11-26 16:57:13 +0100

    jeu de paires lundi apres midi

--- script.js
+++ script.js
@@ -1,75 +1,81 @@
-let uno = document.querySelector(".casella1")
-let due = document.querySelector(".casella2")
-let tre = document.querySelector(".casella3")
-let quattro = document.querySelector(".casella4")
-let cinque = document.querySelector(".casella5")
-let sei = document.querySelector(".casella6")
-let sette = document.querySelector(".casella7")
-let otto = document.querySelector(".casella8")
-let nove = document.querySelector(".casella9")
-let dieci = document.querySelector(".casella10")
-let undici = document.querySelector(".casella11")
-let dodici = document.querySelector(".casella12")
-let tredici = document.querySelector(".casella13")
-let quattordici = document.querySelector(".casella14")
+function global() {
+    let carta = document.getElementsByClassName("carta");
+    let carte = [...carta];
 
-swapImageCounter = 0;
 
-function counter() {
-    swapImageCounter++;
-    return swapImageCounter % 2 == 0;    
-}
+    let annunci = document.querySelector(".testo");
 
-function swapImage() {
-    counter()
-    if (counter() == true) {
-        this.style.backgroundImage = "url('immagini/anehihan.jpg')"
-    } else {
-        this.style.backgroundImage = "url('immagini/coverSmall.jpg')"
+    this.base = '<img src="immagini/coverSmall.jpg" alt="Nascosta" type="nascosta" class="nascosta">';
+    let immagini = [
+        '<img src="immagini/anehihan.jpg" alt="Asino" type="Asino" class="faccie"></img>',
+        '<img src="immagini/anehihan.jpg" alt="Asino" type="Asino" class="faccie"></img>',
+        '<img src="immagini/chatminou.jpg" alt="Gatto" type="Gatto" class="faccie"></img>',
+        '<img src="immagini/chatminou.jpg" alt="Gatto" type="Gatto" class="faccie"></img>',
+        '<img src="immagini/chientoutou.jpg" alt="Cane" type="Cane" class="faccie"></img>',
+        '<img src="immagini/chientoutou.jpg" alt="Cane" type="Cane" class="faccie"></img>',
+        '<img src="immagini/lamacrachat.jpg" alt="Lama" type="Lama" class="faccie"></img>',
+        '<img src="immagini/lamacrachat.jpg" alt="Lama" type="Lama" class="faccie"></img>',
+        '<img src="immagini/lapinscrottes.jpg" alt="Coniglio" type="Coniglio" class="faccie"></img>',
+        '<img src="immagini/lapinscrottes.jpg" alt="Coniglio" type="Coniglio" class="faccie"></img>',
+        '<img src="immagini/lionnegraou.jpg" alt="Leone" type="Leone" class="faccie"></img>',
+        '<img src="immagini/lionnegraou.jpg" alt="Leone" type="Leone" class="faccie"></img>',
+        '<img src="immagini/oursbaby.jpg" alt="Orso" type="Orso" class="faccie"></img>',
+        '<img src="immagini/oursbaby.jpg" alt="Orso" type="Orso" class="faccie"></img>',
+    ]
+
+    function mischia() {
+        immagini = immagini.sort(function (a, b) {
+            return 0.5 - Math.random()
+        });
+        for (i = 0; i < carte.length; i++) {
+            carte[i].innerHTML = base + immagini[i];
+        }
     }
+
+    function swapImage() {
+
+        let scoperte =[]
+
+        if (this.classList.contains("scoperta") == false) {
+            console.log(this)
+            this.classList.add("scoperta");
+            this.children[0].style.zIndex = -1;
+            annunci.innerHTML = "calm thine mammaries";
+            scoperte.push(this.children[1]);
+            if (scoperte.length = 2) {
+                if (scoperte[0].type == scoperte[1].type) {
+                    console.log("correct match works");
+                }
+            }
+
+        } else {
+            console.log("?");
+            annunci.innerHTML = "You already uncovered this image dimwit."
+        }
+    }
+
+    for (i = 0; i < carte.length; i++) {
+        carte[i].addEventListener("click", swapImage);
+    }
+
+    window.onload = mischia();
+
+    //https://medium.freecodecamp.org/vanilla-javascript-tutorial-build-a-memory-game-in-30-minutes-e542c4447eae
+
+    /*
+    uno.addEventListener("click", swapImage);
+    uno.addEventListener("click", counter);
+
+    swapImageCounter = 0;
+
+    function counter() {
+        swapImageCounter++;
+        return swapImageCounter % 2 == 0;    
+    }
+
+
+
+    */
 }
-//https://medium.freecodecamp.org/vanilla-javascript-tutorial-build-a-memory-game-in-30-minutes-e542c4447eae
-uno.addEventListener("click", swapImage);
-uno.addEventListener("click", counter);
 
-due.addEventListener("click", swapImage);
-due.addEventListener("click", counter);
-
-tre.addEventListener("click", swapImage);
-tre.addEventListener("click", counter);
-
-quattro.addEventListener("click", swapImage);
-quattro.addEventListener("click", counter);
-
-cinque.addEventListener("click", swapImage);
-cinque.addEventListener("click", counter);
-
-sei.addEventListener("click", swapImage);
-sei.addEventListener("click", counter);
-
-sette.addEventListener("click", swapImage);
-sette.addEventListener("click", counter);
-
-otto.addEventListener("click", swapImage);
-otto.addEventListener("click", counter);
-
-nove.addEventListener("click", swapImage);
-nove.addEventListener("click", counter);
-
-nove.addEventListener("click", swapImage);
-nove.addEventListener("click", counter);
-
-dieci.addEventListener("click", swapImage);
-dieci.addEventListener("click", counter);
-
-undici.addEventListener("click", swapImage);
-undici.addEventListener("click", counter);
-
-dodici.addEventListener("click", swapImage);
-dodici.addEventListener("click", counter);
-
-tredici.addEventListener("click", swapImage);
-tredici.addEventListener("click", counter);
-
-quattordici.addEventListener("click", swapImage);
-quattordici.addEventListener("click", counter);
+window.onload = global();
